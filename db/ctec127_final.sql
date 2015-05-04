17c4ef18ff3fe8234443e38f892c47a1c2488bd9
Isobel Lennox <isolennox@gmail.com>
2015-05-04 16:11:10 -0700

    Update ctec127_final.sql

--- db/ctec127_final.sql
+++ db/ctec127_final.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.3.8
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 05, 2015 at 09:46 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: May 04, 2015 at 06:05 PM
+-- Server version: 5.5.42-37.1
+-- PHP Version: 5.4.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ctec127_final`
+-- Database: `ilennox_ctec127`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `headers`
+--
+
+CREATE TABLE IF NOT EXISTS `headers` (
+  `id` int(11) NOT NULL,
+  `filepath` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -27,22 +38,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `home_page_content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(900) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `id` int(11) NOT NULL,
+  `content` varchar(900) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `home_page_content`
 --
 
 INSERT INTO `home_page_content` (`id`, `content`) VALUES
-(1, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem odit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem odit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem odit.'),
+(1, 'Hi'),
 (2, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem odit.'),
 (3, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis dolorem, maiores culpa accusantium sit qui aperiam quam veniam in ex commodi nostrum omnis, suscipit inventore. Recusandae quae soluta autem odit.'),
 (4, 'uploads/04_03_1-Stock-Market-Prices_web.jpg'),
 (5, 'uploads/Capture.PNG'),
-(6, 'uploads/cityheader.jpg');
+(6, '6');
 
 -- --------------------------------------------------------
 
@@ -51,21 +61,11 @@ INSERT INTO `home_page_content` (`id`, `content`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `datetime` varchar(60) NOT NULL,
   `content` varchar(900) NOT NULL,
-  `title` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Dumping data for table `news`
---
-
-INSERT INTO `news` (`id`, `datetime`, `content`, `title`) VALUES
-(5, 'February 26, 2015 a	 4:07pm', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro et laboriosam animi repellat corporis, fugiat perspiciatis laudantium consequuntur totam accusantium ea voluptatem, mollitia minima aliquam. Nulla temporibus facilis autem iste. ', 'Net Neutrality'),
-(8, 'February 26, 2015  7:13pm', 'no more a!', 'Fixed timestamp'),
-(9, 'February 26, 2015  7:54pm', 'News Page is up and running!', 'Big News');
+  `title` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -74,16 +74,15 @@ INSERT INTO `news` (`id`, `datetime`, `content`, `title`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `first_name` varchar(60) NOT NULL,
   `last_name` varchar(60) NOT NULL,
   `address` varchar(250) NOT NULL,
   `city` varchar(60) NOT NULL,
   `state` varchar(2) NOT NULL,
   `zip` varchar(11) NOT NULL,
-  `shipped` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `shipped` int(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
@@ -91,11 +90,15 @@ CREATE TABLE IF NOT EXISTS `orders` (
 
 INSERT INTO `orders` (`id`, `first_name`, `last_name`, `address`, `city`, `state`, `zip`, `shipped`) VALUES
 (1, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
-(2, 'Janet', 'Jackson', 'kkfsjsfh', 'ldhf', 'wa', '98771', 0),
-(3, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 0),
-(4, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 0),
-(5, 'ad', 'sdg', 'sgsdg', 'sgdsd', 'wa', '98661', 0),
-(6, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1);
+(2, 'Janet', 'Jackson', 'kkfsjsfh', 'ldhf', 'wa', '98771', 1),
+(3, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
+(4, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
+(5, 'ad', 'sdg', 'sgsdg', 'sgdsd', 'wa', '98661', 1),
+(6, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
+(7, 'Isobel', 'Lennox', '6423 cankse ln', 'vancouver', 'wa', '98664', 1),
+(8, 'sdfsdf', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
+(9, 'Isobel', 'Lennox', '5465 Drury Lane', 'vancouver', 'wa', '98664', 1),
+(10, 'dfhdh', 'dfhdfh', 'dfhdfh', 'dfhd', 'wa', '34534', 1);
 
 -- --------------------------------------------------------
 
@@ -120,7 +123,16 @@ INSERT INTO `orders_products` (`order_id`, `product_id`) VALUES
 (4, 2),
 (5, 3),
 (6, 3),
-(6, 2);
+(6, 2),
+(7, 2),
+(8, 2),
+(8, 3),
+(9, 3),
+(9, 2),
+(9, 3),
+(10, 3),
+(10, 2),
+(10, 3);
 
 -- --------------------------------------------------------
 
@@ -129,21 +141,12 @@ INSERT INTO `orders_products` (`order_id`, `product_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` varchar(500) NOT NULL,
   `filepath` varchar(60) NOT NULL,
-  `price` varchar(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `name`, `description`, `filepath`, `price`) VALUES
-(2, 'Romulan Poster', 'Defeat the Federation with inspiration from this Romulan poster!!', 'uploads/romulaa1n.png', '$35.00'),
-(3, 'Infected USB', 'Terrorize your acquaintances with this infected USB flash drive! ', 'uploads/0-usb_stick.jpg', '$45.00');
+  `price` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -152,21 +155,12 @@ INSERT INTO `products` (`id`, `name`, `description`, `filepath`, `price`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `social` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(60) NOT NULL,
   `avatar` varchar(60) NOT NULL,
   `datetime` varchar(60) NOT NULL,
-  `content` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `social`
---
-
-INSERT INTO `social` (`id`, `name`, `avatar`, `datetime`, `content`) VALUES
-(2, 'Zora', 'uploads/20140304232012.jpg', 'February 26, 2015 a	 6:58pm', 'Bubble Guppies!'),
-(3, 'Romulan', 'uploads/romulaan.png', 'February 27, 2015 a	 6:32pm', 'Peace and Long Life, Nimoy <3');
+  `content` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -175,11 +169,10 @@ INSERT INTO `social` (`id`, `name`, `avatar`, `datetime`, `content`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `subscribed` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subscribed`
@@ -188,8 +181,95 @@ CREATE TABLE IF NOT EXISTS `subscribed` (
 INSERT INTO `subscribed` (`id`, `email`, `name`) VALUES
 (1, 'isolennox@gmail.com', 'Isobel'),
 (2, 'ilennox@clark.edu', 'Romulan'),
-(3, 'techhub@clark.edu', 'Romulan');
+(3, 'techhub@clark.edu', 'Romulan'),
+(4, 'belgort@clark.edu', 'Frank'),
+(5, 'iudf@kjhsg.', 'isobel');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `headers`
+--
+ALTER TABLE `headers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `home_page_content`
+--
+ALTER TABLE `home_page_content`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `social`
+--
+ALTER TABLE `social`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribed`
+--
+ALTER TABLE `subscribed`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `headers`
+--
+ALTER TABLE `headers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `home_page_content`
+--
+ALTER TABLE `home_page_content`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `social`
+--
+ALTER TABLE `social`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subscribed`
+--
+ALTER TABLE `subscribed`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
